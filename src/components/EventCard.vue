2e60336d5b1ee5b553cc321e062ea3c204260d29
XDMelone333 <till.schmidt333@gmx.net>
2022-11-17 17:23:20 +0100

    First Steps for final Application 2

---
+++ src/components/EventCard.vue
@@ -0,0 +1,23 @@
+<script>
+
+export default {
+  data () {
+    return {
+        
+    }
+  }
+}
+
+const eventStartDate = computed(() => new Date(props.event.doorsOpen).toLocaleDateString("de-DE"));
+const eventStartTime = computed(() =>
+  new Date(props.event.doorsOpen).toLocaleTimeString("de-DE", {
+    hour: "2-digit",
+    minute: "2-digit",
+  })
+);
+
+function formatPrice(price: Price) {
+  if (price.currency === "EUR") return `${price.value} €`;
+  else return `${price.value} ${price.currency}`;
+}
+</script>
