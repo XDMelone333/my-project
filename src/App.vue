7e2ca029b8d62ac07adf4d0f6752bc3404dbb84f
Fuat Taneri <fuat.taneri@eventim.de>
2022-11-18 14:44:49 +0100

    Just a quick backup

--- src/App.vue
+++ src/App.vue
@@ -5,102 +5,59 @@ import axios from 'axios'
 export default {
     data () {
     return {
-        content: []
+        content: [],
+        loading: false,
+        selection: 1,
     }
   },
   methods: {
     async MakeRequest () {
       const content = await axios.get('https://corsproxy.io/?https%3A%2F%2Fwww.eventim-light.com%2Fde%2Fa%2F5da03c56503ca200015df6cb%2Fapi%2Fevent')
       this.content = content.data
+    },
+    reserve () {
+      this.loading = true
+
+      setTimeout(() => (this.loading = false), 2000)
     }
   },
   mounted () {
     this.MakeRequest ()
   },
 }
-
 </script>
-
 <template>
-  <v-app>
-    <h1>Hello World!</h1>
-    <div>
+  <div>
+    <v-app id="inspire">
       <v-card
-      :loading="loading"
-      class="mx-auto my-12"
-      max-width="374"
-    >
-      <template slot="progress">
-        <v-progress-linear
-          color="deep-purple"
-          height="10"
-          indeterminate
-        ></v-progress-linear>
-      </template>
-  
-      <v-img
-        height="250"
-        src="https://cdn.vuetifyjs.com/images/cards/cooking.png"
-      ></v-img>
-  
-      <v-card-title>Cafe Badilico</v-card-title>
-  
-      <v-card-text>
-        <v-row
-          align="center"
-          class="mx-0"
-        >
-          <v-rating
-            :value="4.5"
-            color="amber"
-            dense
-            half-increments
-            readonly
-            size="14"
-          ></v-rating>
-  
-          <div class="grey--text ms-4">
-            4.5 (413)
-          </div>
-        </v-row>
-  
-        <div class="my-4 text-subtitle-1">
-          $ • Italian, Cafe
-        </div>
-  
-        <div>Small plates, salads & sandwiches - an intimate setting with 12 indoor seats plus patio seating.</div>
-      </v-card-text>
-  
-      <v-divider class="mx-4"></v-divider>
-  
-      <v-card-title>Tonight's availability</v-card-title>
-  
-      <v-card-text>
-        <v-chip-group
-          v-model="selection"
-          active-class="deep-purple accent-4 white--text"
-          column
-        >
-          <v-chip>5:30PM</v-chip>
-  
-          <v-chip>7:30PM</v-chip>
-  
-          <v-chip>8:00PM</v-chip>
-  
-          <v-chip>9:00PM</v-chip>
-        </v-chip-group>
-      </v-card-text>
-  
-      <v-card-actions>
-        <v-btn
-          color="deep-purple lighten-2"
-          text
-          @click="reserve"
-        >
-          Reserve
-        </v-btn>
-      </v-card-actions>
-    </v-card>
-    </div>
-</v-app>
+          :loading="loading"
+          class="mx-auto my-12"
+          max-width="374"
+      >
+        <template slot="progress">
+          <v-progress-linear
+              color="deep-purple"
+              height="10"
+              indeterminate
+          ></v-progress-linear>
+        </template>
+        <v-img
+            height="250"
+            src="https://www.eventim-light.com/de/api/image/5fb790f208559a0a31c427bb/shop_preview/webp"
+        ></v-img>
+        <v-card-title>Wohl bekloppt geworden</v-card-title>
+        <p class="my-0 text-subtitle-1"></p>
+        <v-card-subtitle>Die Comedy Mix Show im Atelier Theater</v-card-subtitle>
+        <v-divider class="mx-4"></v-divider>
+        <v-card-text>
+          <v-icon small>event</v-icon> 19.11.2022 <font-awesome-icon icon="fa-solid fa-calendar" class="me-2" /> 19:30
+        </v-card-text>
+        <v-card-actions>
+          <v-btn color="green lighten-1" @click="reserve">
+            From 17.55€
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-app>
+  </div>
 </template>
