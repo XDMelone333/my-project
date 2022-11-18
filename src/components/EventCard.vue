0231ae79715794201e936fafb52d618263124aef
XDMelone333 <till.schmidt333@gmx.net>
2022-11-18 11:12:24 +0100

    In the middle of the progress

--- src/components/EventCard.vue
+++ src/components/EventCard.vue
@@ -7,17 +7,3 @@ export default {
     }
   }
 }
-
-const eventStartDate = computed(() => new Date(props.event.doorsOpen).toLocaleDateString("de-DE"));
-const eventStartTime = computed(() =>
-  new Date(props.event.doorsOpen).toLocaleTimeString("de-DE", {
-    hour: "2-digit",
-    minute: "2-digit",
-  })
-);
-
-function formatPrice(price: Price) {
-  if (price.currency === "EUR") return `${price.value} €`;
-  else return `${price.value} ${price.currency}`;
-}
-</script>
--- src/components/EventList.vue
+++ src/components/EventList.vue
@@ -1,34 +1,23 @@
 <script>
 
 // import EventCard from "./EventCard.vue";
-import axios from 'axios'
+
+import EventContent from './EventContent.vue'
 
 export default {
   components: {
 //    EventCard,
-  },
-  methods: {
-    async MakeRequest () {
-      const content = await axios.get('https://corsproxy.io/?https%3A%2F%2Fwww.eventim-light.com%2Fde%2Fa%2F5da03c56503ca200015df6cb%2Fapi%2Fevent')
-      this.content = content.data
-    }
-  },
-  mounted () {
-    this.MakeRequest ()
-  },
-  data () {
-    return {
-        content: []
-    }
+    EventContent
   }
 };
 
 </script>
 <template>
   <v-app>
-      <div v-for="event in content" :key="event.id" class="col-12 col-md-6 col-xl-4">
+      <div v-for="event in content" :key="event.id">
         <li>
-            {{ event.id }}
+          {{ event.id }}
+          <EventContent/>
         </li>
     </div>
   </v-app>
