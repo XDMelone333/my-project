b4217107049e3005b4960da33141f8fa69db8b26
XDMelone333 <till.schmidt333@gmx.net>
2022-11-21 17:01:04 +0100

    implemented dynamic data input

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,41 @@
 <script>
+  import axios from 'axios'
+  let urlParams = new URLSearchParams(window.location.search);
+  let myParam = urlParams.get('api');
 
-import axios from 'axios'
-
-export default {
-    data () {
-    return {
-        content: [],
-        loading: false,
-        selection: 1,
-    }
-  },
-  methods: {
-    async MakeRequest () {
-      const content = await axios.get('https://corsproxy.io/?https%3A%2F%2Fwww.eventim-light.com%2Fde%2Fa%2F5da03c56503ca200015df6cb%2Fapi%2Fevent')
-      this.content = content.data
+  export var apiUrl = `https://corsproxy.io/?https%3A%2F%2Fwww.eventim-light.com%2Fde%2Fa%2F${myParam}%2Fapi%2Fevent`;
+  export default {
+      data () {
+      return {
+          content: [],
+          loading: false,
+          selection: 1,
+      }
     },
-    reserve () {
-      this.loading = true
+    methods: {
+      async MakeRequest () {
+          const content = await axios.get(apiUrl)
+          this.content = content.data
+      },
+      reserve () {
+        this.loading = true
 
-      setTimeout(() => (this.loading = false), 2000)
+        setTimeout(() => (this.loading = false), 2000)
+      },
+      getImageUrl (id) {
+        return `https://www.eventim-light.com/de/api/image/${id}/shop_preview/webp`
+      },
+      formatPrice (price) {
+        if (price.currency === "EUR") return `${price.value} €`
+        else return `${price.value} ${price.currency}`
+      }
+    },
+    mounted () {
+      this.MakeRequest ()
     }
-  },
-  mounted () {
-    this.MakeRequest ()
-  },
+
 }
+
 </script>
 <template>
   <div>
@@ -46,24 +57,30 @@ export default {
               >
               </v-progress-linear>
             </template>
-            <v-img height="250" src="https://www.eventim-light.com/de/api/image/5fb790f208559a0a31c427bb/shop_preview/webp"></v-img>
-            <figcaption class="mx-3 mt-1 text-end grey--text" :style="{'font-size': '12px'}">Image: Christof Leim</figcaption>
+            <v-img height="250" :src="getImageUrl(event.image.id)"></v-img>
+            <figcaption class="mx-3 mt-1 text-end grey--text" :style="{'font-size': '12px'}">Image: {{ event.image.copyright }}</figcaption>
             <div class="white--text">
-              <h3 class="mx-4">Wohl bekloppt geworden</h3>
-              <v-card-subtitle class="mt-0">Die Comedy Mix Show im Atelier Theater</v-card-subtitle>
+              <h3 class="mx-4">{{ event.title }}</h3>
+              <v-card-subtitle class="mt-0">{{ event.subtitle }}</v-card-subtitle>
               <v-divider class="mx-4"></v-divider>
               <v-card-text>
-                <font-awesome-icon icon="fa-solid fa-calendar" class="me-1"/> 19.11.2022
-                <font-awesome-icon icon="fa-solid fa-clock" class="ms-2" /> 19:30
+                <font-awesome-icon icon="fa-solid fa-calendar" class="me-1"/> {{ new Date(event.doorsOpen).toLocaleDateString("de-DE") }}
+                <font-awesome-icon icon="fa-solid fa-clock" class="ms-2" /> {{ new Date(event.doorsOpen).toLocaleTimeString("de-DE", {hour: "2-digit", minute: "2-digit"}) }}
                 <div class="my-2">
-                  <font-awesome-icon icon="fa-solid fa-location-dot" class="me-1"/> Atelier Theater, Köln
+                  <font-awesome-icon icon="fa-solid fa-location-dot" class="me-1"/> {{ event.venue.name }}, {{ event.venue.city }}
                 </div>
               </v-card-text>
             </div>
             <v-card-actions>
-              <v-btn color="green" @click="reserve">
-                From 17.55€
-              </v-btn>
+              <template v-if="event.soldout">
+                <v-btn depressed color="green--text">Sold out</v-btn>
+              </template>
+              <template v-else-if="!event.salesStart || event.salesStart < new Date().toISOString()">
+                <v-btn color="green" @click="reserve"> From {{ formatPrice(event.minPrice) }} </v-btn>
+              </template>
+              <template v-else>
+                <v-btn color="green darken-4 grey--text">Coming soon...</v-btn>
+              </template>
             </v-card-actions>
           </v-card>
         </v-col>
